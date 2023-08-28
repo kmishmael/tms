65db710684ed421769f4bab466a5d0cf778aef6c
kibet <kmishmael@gmail.com>
2023-08-28 11:17:54 +0300

    react router setup

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+export default function Home() {
   const [count, setCount] = useState(0)
 
   return (
@@ -31,5 +30,3 @@ function App() {
     </>
   )
 }
-
-export default App
