55c07855f0cdec39370e922d923e61da1b1a56ee
kibet <kmishmael@gmail.com>
2023-08-29 21:29:11 +0300

    fix dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useNavigate,
-} from "react-router-dom";
-import Navbar from "./components/navbar";
-import Sidebar from "./components/sidebar";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+
 import Dashboard from "./components/edit-ticket";
 import CreateTicket from "./components/create-ticket";
 import ManageUsers from "./components/manage-users";
@@ -14,6 +8,7 @@ import ManageProjects from "./components/manage-project";
 import EditTicket from "./components/edit-ticket";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { ClerkProvider, SignIn, SignUp } from "@clerk/clerk-react";
+import Layout from "./components/layout";
 
 if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
@@ -25,40 +20,74 @@ function ClerkProviderWithRoutes() {
   const navigate = useNavigate();
 
   return (
-      <ClerkProvider
-        publishableKey={clerkPubKey}
-        navigate={(to) => navigate(to)}
-      >
-        <Navbar />
-        <div className="wrapp flex w-full">
-          <Sidebar />
-          <div id="content">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route
-                path="/sign-in/*"
-                element={<SignIn routing="path"  path="/sign-in" />}
-              />
-              <Route
-                path="/sign-up/*"
-                element={<SignUp routing="path" path="/sign-up" />}
-              />
-              <Route path="/tickets/create" element={<CreateTicket />} />
-              <Route path="/manage-users" element={<ManageUsers />} />
-              <Route path="/users/create" element={<CreateUser />} />
-              <Route path="/manage-projects" element={<ManageProjects />} />
-              <Route path="/edit/:id" element={<EditTicket />} />
-            </Routes>
-          </div>
-        </div>
-      </ClerkProvider>
+    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+
+        <Route
+          path="/tickets/create"
+          element={
+            <Layout>
+              <CreateTicket />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-users"
+          element={
+            <Layout>
+              <ManageUsers />
+            </Layout>
+          }
+        />
+        <Route
+          path="/users/create"
+          element={
+            <Layout>
+              <CreateUser />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-projects"
+          element={
+            <Layout>
+              <ManageProjects />
+            </Layout>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <Layout>
+              <EditTicket />
+            </Layout>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
   );
 }
 
 export default function App() {
   return (
     <BrowserRouter>
-    <ClerkProviderWithRoutes />
+      <ClerkProviderWithRoutes />
     </BrowserRouter>
-  )
+  );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,14 @@
-export default function Layout() {
-    return (
-        <>
-        </>
-    )
+import Navbar from "./navbar";
+import Sidebar from "./sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <div className="wrapp flex w-full">
+        <Sidebar />
+        <div id="content">{children}</div>
+      </div>
+    </>
+  );
 }
