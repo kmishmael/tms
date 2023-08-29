c41ad1519427c257b63f5b04b3937aad9d696e95
kibet <kmishmael@gmail.com>
2023-08-29 18:14:48 +0300

    fighting git

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
 import Dashboard from "./components/edit-ticket";
@@ -7,26 +12,46 @@ import ManageUsers from "./components/manage-users";
 import CreateUser from "./components/create-user";
 import ManageProjects from "./components/manage-project";
 import EditTicket from "./components/edit-ticket";
-import Home from "./routes/home";
-//import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { ClerkProvider, SignIn, SignUp } from "@clerk/clerk-react";
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 export default function App() {
+
   return (
-    <Router>
-      <Navbar />
-      <div className="wrapper">
-        <Sidebar />
-        <div id="content">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/tickets/create" element={<CreateTicket />} />
-            <Route path="/manage-users" element={<ManageUsers />} />
-            <Route path="/users/create" element={<CreateUser />} />
-            <Route path="/manage-projects" element={<ManageProjects />} />
-            <Route path="/edit/:id" element={<EditTicket />} />
-          </Routes>
+    <BrowserRouter>
+      <ClerkProvider
+        publishableKey={clerkPubKey}
+        navigate={(to) => navigate(to)}
+      >
+        <Navbar />
+        <div className="wrapper">
+          <Sidebar />
+          <div id="content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route
+                path="/sign-in/*"
+                element={<SignIn routing="path"  path="/sign-in" />}
+              />
+              <Route
+                path="/sign-up/*"
+                element={<SignUp routing="path" path="/sign-up" />}
+              />
+              <Route path="/tickets/create" element={<CreateTicket />} />
+              <Route path="/manage-users" element={<ManageUsers />} />
+              <Route path="/users/create" element={<CreateUser />} />
+              <Route path="/manage-projects" element={<ManageProjects />} />
+              <Route path="/edit/:id" element={<EditTicket />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </ClerkProvider>
+    </BrowserRouter>
   );
 }
