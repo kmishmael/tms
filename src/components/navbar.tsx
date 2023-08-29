35e5d0413bf096778adbdd9b8c4626a6c665979e
kibet <kmishmael@gmail.com>
2023-08-29 22:03:34 +0300

    add authentication

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,31 @@
+import { SignOutButton, useUser } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { isSignedIn } = useUser();
+
   return (
-    <nav className="navbar navbar-light bg-light navbar-expand-lg ml-auto">
-      <div className="container-fluid">
-        <button
-          className="btn btn-dark d-inline-block d-lg-none ml-auto"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-align-justify"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li>
-              <Link to="/sign-in" className="nav-link">
-                Log In
-              </Link>
-            </li>
-            <li>
-              <Link to="/sign-up" className="nav-link">
-                Sign Out
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar flex justify-end navbar-light bg-light navbar-expand-lg ml-auto">
+      <div className="ml-auto mr-16">
+        
+        {isSignedIn ? (
+          <SignOutButton />
+        ) : (
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li>
+                <Link to="/sign-in" className="nav-link">
+                  Log In
+                </Link>
+              </li>
+              <li>
+                <Link to="/sign-up" className="nav-link">
+                  Sign Out
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
